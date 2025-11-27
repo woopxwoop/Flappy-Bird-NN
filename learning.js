51e47c2561ac87e1968d69404e687b902bc83410
An Lou <louan1734@gmail.com>
2025-11-27 16:37:32 -0600

    init

---
+++ learning.js
@@ -0,0 +1,96 @@
+/*
+copy paste all the code from learning2.js for it to work
+(or switch src in index.html to learning2)
+*/
+
+//this is a constructor i promise i know it doesn't look like it
+function neuron() {
+  this.weights = [];
+
+  this.predict = (input) => {
+    while (this.weights.length <= input.length) {
+      // init random weights + 1 bias
+      this.weights.push(Math.random() * 2 - 1);
+    }
+
+    let sum = 0;
+
+    // add weights * inputs
+    for (let i = 0; i < input.length; i++) {
+      sum += this.weights[i] * input[i];
+    }
+
+    sum += this.weights[this.weights.length - 1]; // add bias
+
+    if (sum > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+}
+
+//yet another confusing constructor (🔥🔥🔥🔥)
+function layer(numNeurons) {
+  this.neurons = [];
+
+  for (let i = 0; i < numNeurons; i++) {
+    this.neurons.push(new neuron());
+  }
+
+  this.predict = (input) => {
+    let output = [];
+
+    for (let neuron of this.neurons) {
+      output.push(neuron.predict(input));
+    }
+
+    return output;
+  };
+}
+
+//the whole network! (⚡️⚡️⚡️ constructor)(using ⚡️ instead of 🔥 to save the environment)
+function net() {
+  this.layers = [];
+
+  this.init = () => {
+    this.layers.push(new layer(3));
+    this.layers.push(new layer(3));
+    this.layers.push(new layer(1));
+  };
+
+  this.predict = (input) => {
+    for (let layer of this.layers) {
+      input = layer.predict(input);
+    }
+
+    return input[0] > 0; // will flap if > 0
+  };
+
+  this.init();
+}
+
+/*–
+this is not a constructor, just a function which
+creates a mutated copy of another neural net (ideally a parent)
+(⚡️⚡️⚡️⚡️)
+*/
+function mutate(parentNet) {
+  let mutation = new net();
+
+  for (let i = 0; i < parentNet.layers.length; i++) {
+    for (let j = 0; j < parentNet.layers[i].neurons.length; j++) {
+      for (let k = 0; k < parentNet.layers[i].neurons[j].weights.length; k++) {
+        let mutationRate = 0.05;
+
+        if (Math.random() <= mutationRate) {
+          mutation.layers[i].neurons[j].weights[k] = Math.random() * 2 - 1;
+        } else {
+          mutation.layers[i].neurons[j].weights[k] =
+            parentNet.layers[i].neurons[j].weights[k];
+        }
+      }
+    }
+  }
+  return mutation;
+}
---
+++ main.js
@@ -0,0 +1,300 @@
+let c = document.createElement("canvas");
+let c2 = document.createElement("canvas");
+
+document.body.appendChild(c);
+//document.body.appendChild(c2);
+
+let scale = 2;
+c.width = scale * window.innerWidth;
+c.height = scale * window.innerHeight;
+
+c2.width = scale * window.innerWidth;
+c2.height = scale * window.innerHeight;
+
+let ctx = c.getContext("2d");
+
+let birds = [];
+let pipes = [];
+
+let pipeTimer = 1500;
+
+let frontPipe;
+
+let deadBirds = [];
+
+let best;
+
+let gen = 1;
+
+function bird() {
+  birds.push(this);
+  this.score = 0;
+  this.color = "rgba(0, 0, 0, 0.2)";
+  this.y = 0.5;
+  this.x = 0;
+  this.yVelocity = 0;
+  this.brain = new net();
+  this.gravity = 0.0003;
+  this.dead = false;
+  this.update = () => {
+    if (!this.dead) {
+      this.score++;
+    }
+    this.yVelocity += this.gravity;
+    this.y += this.yVelocity;
+    if (
+      this.brain.predict([
+        this.y,
+        this.yVelocity,
+        frontPipe.height,
+        frontPipe.x,
+      ])
+    ) {
+      this.yVelocity = -0.008; //flap
+    }
+    if (this.y > 1 || this.y < 0) {
+      if (!this.dead) {
+        deadBirds.push(this);
+      }
+      this.dead = true;
+      return false;
+    }
+    for (p of pipes) {
+      if (
+        this.x > p.x &&
+        this.x < p.x + 0.1 &&
+        Math.abs(this.y - p.height) > 0.1
+      ) {
+        if (!this.dead) {
+          deadBirds.push(this);
+        }
+        this.dead = true;
+        return false;
+      }
+    }
+
+    return true;
+  };
+  this.draw = () => {
+    if (!this.dead) {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(
+        (c.width - c.height) / 2,
+        this.y * c.height,
+        c.height * 0.025,
+        0,
+        Math.PI * 2
+      );
+      ctx.fill();
+      ctx.closePath();
+    }
+  };
+}
+
+function pipe() {
+  pipes.push(this);
+  this.x = (c.width - c.height) / 2 / c.height + 1;
+  this.color = "black";
+  this.height = Math.random() * 0.75 + 0.125;
+  this.update = () => {
+    this.x -= 0.01;
+    if (this.x < -((c.width - c.height) / 2) / c.height - 0.1) {
+      pipes.shift();
+      return false;
+    }
+    return true;
+  };
+  this.draw = () => {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(
+      this.x * c.height + (c.width - c.height) / 2,
+      0,
+      c.height * 0.1,
+      c.height * (this.height - 0.1)
+    );
+    ctx.fillRect(
+      this.x * c.height + (c.width - c.height) / 2,
+      (this.height + 0.1) * c.height,
+      c.height * 0.1,
+      c.height
+    );
+  };
+}
+
+function drawBrain(brain) {
+  for (let i = brain.layers.length - 1; i >= 0; i--) {
+    let yStep = 1 / (1 + brain.layers[i].neurons.length);
+    let lineStep;
+    if (i != 0) {
+      lineStep = 1 / (1 + brain.layers[i - 1].neurons.length);
+    } else {
+      lineStep = 1 / 5;
+    }
+
+    for (let j = 1; j * yStep < 1; j++) {
+      for (let k = 1; k * lineStep < 1; k++) {
+        ctx.beginPath();
+        ctx.lineWidth =
+          Math.abs(brain.layers[i].neurons[j - 1].weights[k - 1]) *
+          c.height *
+          0.005;
+        if (brain.layers[i].neurons[j - 1].weights[k - 1] > 0) {
+          ctx.strokeStyle = "red";
+        } else {
+          ctx.strokeStyle = "blue";
+        }
+        ctx.moveTo(
+          (c.width / 4) * (i / brain.layers.length) + (c.width * 3) / 4,
+          (c.height / 3) * j * yStep
+        );
+        ctx.lineTo(
+          (c.width / 4) * ((i - 1) / brain.layers.length) + (c.width * 3) / 4,
+          (c.height / 3) * k * lineStep
+        );
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+
+    for (let j = 1; j * yStep < 1; j++) {
+      ctx.beginPath();
+      ctx.arc(
+        (c.width / 4) * (i / brain.layers.length) + (c.width * 3) / 4,
+        (c.height / 3) * j * yStep,
+        c.height * 0.01,
+        0,
+        Math.PI * 2
+      );
+      ctx.fillStyle = "black";
+      ctx.fill();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.arc(
+        (c.width / 4) * (i / brain.layers.length) + (c.width * 3) / 4,
+        (c.height / 3) * j * yStep,
+        c.height * 0.01 * 0.5,
+        0,
+        Math.PI * 2
+      );
+      ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+      ctx.fill();
+      ctx.closePath();
+
+      let rad =
+        Math.abs(
+          brain.layers[i].neurons[j - 1].weights[
+            brain.layers[i].neurons[j - 1].weights.length - 1
+          ]
+        ) / 2;
+      if (
+        brain.layers[i].neurons[j - 1].weights[
+          brain.layers[i].neurons[j - 1].weights.length - 1
+        ] > 0
+      ) {
+        ctx.fillStyle = "red";
+      } else {
+        ctx.fillStyle = "blue";
+      }
+
+      ctx.beginPath();
+      ctx.arc(
+        (c.width / 4) * (i / brain.layers.length) + (c.width * 3) / 4,
+        (c.height / 3) * j * yStep,
+        c.height * 0.01 * rad,
+        0,
+        Math.PI * 2
+      );
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    for (let k = 1; k * lineStep < 1; k++) {
+      ctx.beginPath();
+      ctx.arc(
+        (c.width / 4) * ((i - 1) / brain.layers.length) + (c.width * 3) / 4,
+        (c.height / 3) * k * lineStep,
+        c.height * 0.01,
+        0,
+        Math.PI * 2
+      );
+      ctx.fillStyle = "black";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
+function update() {
+  c.width = scale * window.innerWidth;
+  c.height = scale * window.innerHeight;
+  if (deadBirds.length == birds.length) {
+    gen++;
+    birds.sort((a, b) => {
+      return b.score - a.score;
+    });
+    let parents = [];
+    for (let i = 0; i < 10; i++) {
+      parents.push(birds[i]);
+      birds[i].dead = false;
+      birds[i].y = 0.5;
+      birds[i].yVelocity = 0;
+      birds[i].score = 0;
+    }
+    birds = [];
+    deadBirds = [];
+    pipes = [];
+    for (let i = 0; i < 999; i++) {
+      let newBird = new bird();
+      newBird.brain = mutate(parents[~~(Math.random() * parents.length)].brain);
+    }
+    birds.push(parents[0]);
+    best = parents[0];
+  }
+  pipeTimer += 1000 / 60;
+  if (pipeTimer > 1500) {
+    pipeTimer = 0;
+    new pipe();
+  }
+  for (let i = 0; i < pipes.length; i++) {
+    if (pipes[i].x > -0.1) {
+      frontPipe = pipes[i];
+      break;
+    }
+  }
+  ctx.clearRect(0, 0, c.width, c.height);
+  for (i of birds) {
+    i.update();
+    i.draw();
+  }
+  for (let i = 0; i < pipes.length; i++) {
+    if (!pipes[i].update()) {
+      i--;
+    } else {
+      pipes[i].draw();
+    }
+  }
+  if (best != undefined) {
+    drawBrain(best.brain);
+  } else {
+    drawBrain(birds[0].brain);
+  }
+  ctx.textAlign = "left";
+  ctx.fillStyle = "black";
+  ctx.strokeStyle = "white";
+  ctx.font = "bold " + Math.floor(c.height / 25) + "px arial";
+  ctx.fillText("Generation: " + gen, (c.width * 1) / 50, c.height / 10);
+  ctx.fillText(
+    "Num alive: " + (birds.length - deadBirds.length),
+    (c.width * 1) / 50,
+    c.height / 5
+  );
+
+  ctx.filter = "invert(1)";
+  ctx.drawImage(c, 0, 0);
+}
+
+for (let i = 0; i < 1000; i++) new bird();
+
+setInterval(update, 1000 / 60);
